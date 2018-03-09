3fed2e884b9ab0a74d47dcadeb16a2c8be54af7b
daniel Camacho <danielcamacho@MacBook-Pro-de-daniel.local>
2018-03-09 11:49:36 +0100

    new repository

---
+++ students2/assets/js/main.js
@@ -0,0 +1,15 @@
+function redirect() {
+	var textValue = document.getElementById("text").value;
+	if((textValue == "Conciertos") || (textValue == "conciertos") || (textValue == "concierto") || (textValue == "Concierto"))
+    {
+        location.href = "concert.html"
+    }
+    else if((textValue == "Museos") || (textValue == "museos") || (textValue == "museo") || (textValue == "Museo"))
+    {
+        location.href = "museum.html"
+    }
+    else if((textValue == "Teatros") || (textValue == "teatros") || (textValue == "teatro") || (textValue == "Teatro"))
+    {
+        location.href = "teatro.html"
+    }
+}
